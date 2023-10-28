a1224320df62b508c997cf5c00a518b2c8a8b963
Dr. Jochen Manns <vcrnet-1@psimarron.net>
2023-10-28 14:19:44 +0200

    Added a single profile test.

---
+++ Tests/HardwareAbstraction/ProfileTests.cs
@@ -0,0 +1,16 @@
+using JMS.DVB;
+
+namespace DVBNETTests.HardwareAbstraction;
+
+[TestFixture]
+public class ProfileTests
+{
+    [Test]
+    public void Can_Load_Channel_File()
+    {
+        var profile = ProfileManager.LoadProfile(new FileInfo("TestData/stations.dnp"));
+
+        Assert.That(profile, Is.Not.Null);
+        Assert.That(profile.AllSources.Count, Is.EqualTo(3345));
+    }
+}
