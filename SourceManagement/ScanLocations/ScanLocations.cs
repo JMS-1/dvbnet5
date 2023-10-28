eace6274ecebd1348ca46b72118e9004d4fac866
Dr. Jochen Manns <vcrnet-1@psimarron.net>
2023-10-28 11:25:29 +0200

    Starting to modernize code - at least a bit.

--- SourceManagement/ScanLocations/ScanLocations.cs
+++ SourceManagement/ScanLocations/ScanLocations.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.IO;
-using System.Xml;
+﻿using System.Xml;
 using System.Text;
 using System.Xml.Serialization;
-using System.Collections.Generic;
 
 namespace JMS.DVB
 {
@@ -18,12 +15,12 @@ namespace JMS.DVB
         /// <summary>
         /// Die Standardkonfiguration für den Sendersuchlauf.
         /// </summary>
-        private static ScanLocations m_Default;
+        private static ScanLocations? m_Default;
 
         /// <summary>
         /// Schützt den Zugriff auf die Standardkonfiguration.
         /// </summary>
-        private static object m_DefaultLock = new object();
+        private static readonly object m_DefaultLock = new object();
 
         /// <summary>
         /// Der XML Namensraum für diese .NET Klasse.
@@ -38,16 +35,16 @@ namespace JMS.DVB
         /// <summary>
         /// Die DVB.NET Version, mit der diese Instanz erstellt wurde.
         /// </summary>
-        [XmlAttribute( "version" )]
+        [XmlAttribute("version")]
         public string Version { get; set; }
 
         /// <summary>
         /// Die Ursprünge aller Art.
         /// </summary>
-        [XmlArray( "List" )]
-        [XmlArrayItem( typeof( SatelliteScanLocation ) )]
-        [XmlArrayItem( typeof( CableScanLocation ) )]
-        [XmlArrayItem( typeof( TerrestrialScanLocation ) )]
+        [XmlArray("List")]
+        [XmlArrayItem(typeof(SatelliteScanLocation))]
+        [XmlArrayItem(typeof(CableScanLocation))]
+        [XmlArrayItem(typeof(TerrestrialScanLocation))]
         public readonly List<ScanLocation> Locations = new List<ScanLocation>();
 
         /// <summary>
@@ -63,19 +60,22 @@ namespace JMS.DVB
         /// Legt diese Informationen in einem Datenstrom ab.
         /// </summary>
         /// <param name="stream">Der gewünschte Datenstrom.</param>
-        public void Save( Stream stream )
+        public void Save(Stream stream)
         {
             // Create serializer
-            XmlSerializer serializer = new XmlSerializer( GetType(), Namespace );
+            XmlSerializer serializer = new(GetType(), Namespace);
 
             // Configure format
-            XmlWriterSettings settings = new XmlWriterSettings();
-            settings.Encoding = Encoding.Unicode;
-            settings.Indent = true;
+            XmlWriterSettings settings = new()
+            {
+                Encoding = Encoding.Unicode,
+                Indent = true
+            };
 
             // Store
-            using (XmlWriter writer = XmlWriter.Create( stream, settings ))
-                serializer.Serialize( writer, this );
+            using XmlWriter writer = XmlWriter.Create(stream, settings);
+
+            serializer.Serialize(writer, this);
         }
 
         /// <summary>
@@ -83,56 +83,45 @@ namespace JMS.DVB
         /// </summary>
         /// <param name="path">Der volle Pfad zu Datei.</param>
         /// <param name="mode">Die Art, wie die Datei zu erzeugen ist.</param>
-        public void Save( string path, FileMode mode )
+        public void Save(string path, FileMode mode)
         {
             // Create stream and forward
-            using (FileStream stream = new FileStream( path, mode, FileAccess.Write, FileShare.None ))
-                Save( stream );
+            using FileStream stream = new(path, mode, FileAccess.Write, FileShare.None);
+
+            Save(stream);
         }
 
         /// <summary>
         /// Legt dieser Informationen in einer Datei ab.
         /// </summary>
         /// <param name="path">Der volle Pfad zu Datei.</param>
-        public void Save( string path )
-        {
-            // Forward
-            Save( path, FileMode.Create );
-        }
+        public void Save(string path) => Save(path, FileMode.Create);
 
         /// <summary>
         /// Legt dieser Informationen in einer Datei ab.
         /// </summary>
         /// <param name="path">Die Zieldatei.</param>
         /// <param name="mode">Die Art, wie die Datei zu erzeugen ist.</param>
-        public void Save( FileInfo path, FileMode mode )
-        {
-            // Forward
-            Save( path.FullName, mode );
-        }
+        public void Save(FileInfo path, FileMode mode) => Save(path.FullName, mode);
 
         /// <summary>
         /// Legt dieser Informationen in einer Datei ab.
         /// </summary>
         /// <param name="path">Die Zieldatei.</param>
-        public void Save( FileInfo path )
-        {
-            // Forward
-            Save( path.FullName, FileMode.Create );
-        }
+        public void Save(FileInfo path) => Save(path.FullName, FileMode.Create);
 
         /// <summary>
         /// Rekonstruiert eine Instanz aus einem Datenstrom.
         /// </summary>
         /// <param name="stream">Der Datenstrom mit der XML Repräsentation der Instanz.</param>
         /// <returns>Die gewünschte Instanz.</returns>
-        public static ScanLocations Load( Stream stream )
+        public static ScanLocations Load(Stream stream)
         {
             // Create serializer
-            XmlSerializer serializer = new XmlSerializer( typeof( ScanLocations ), Namespace );
+            XmlSerializer serializer = new(typeof(ScanLocations), Namespace);
 
             // Process
-            return (ScanLocations) serializer.Deserialize( stream );
+            return (ScanLocations)serializer.Deserialize(stream)!;
         }
 
         /// <summary>
@@ -140,11 +129,12 @@ namespace JMS.DVB
         /// </summary>
         /// <param name="path">Der volle Pfad zur Datei mit der XML Repräsentation der Instanz.</param>
         /// <returns>Die gewünschte Instanz.</returns>
-        public static ScanLocations Load( string path )
+        public static ScanLocations Load(string path)
         {
             // Open and forward
-            using (FileStream stream = new FileStream( path, FileMode.Open, FileAccess.Read, FileShare.Read ))
-                return Load( stream );
+            using FileStream stream = new(path, FileMode.Open, FileAccess.Read, FileShare.Read);
+
+            return Load(stream);
         }
 
         /// <summary>
@@ -152,11 +142,7 @@ namespace JMS.DVB
         /// </summary>
         /// <param name="file">Die Datei mit der XML Repräsentation der Instanz.</param>
         /// <returns>Die gewünschte Instanz.</returns>
-        public static ScanLocations Load( FileInfo file )
-        {
-            // Forward
-            return Load( file.FullName );
-        }
+        public static ScanLocations Load(FileInfo file) => Load(file.FullName);
 
         /// <summary>
         /// Erzeugt eine exakte Kopie.
@@ -165,17 +151,16 @@ namespace JMS.DVB
         public ScanLocations Clone()
         {
             // Use helper
-            using (MemoryStream stream = new MemoryStream())
-            {
-                // Put in
-                Save( stream );
+            using MemoryStream stream = new();
 
-                // Reset stream to the very beginning
-                stream.Seek( 0, SeekOrigin.Begin );
+            // Put in
+            Save(stream);
 
-                // Recreate
-                return Load( stream );
-            }
+            // Reset stream to the very beginning
+            stream.Seek(0, SeekOrigin.Begin);
+
+            // Recreate
+            return Load(stream);
         }
 
         /// <summary>
@@ -186,26 +171,26 @@ namespace JMS.DVB
         private static ScanLocations Load()
         {
             // Load it
-            ScanLocations product = new ScanLocations();
+            ScanLocations product = new();
 
             // Attach to the path of the scan locations
-            DirectoryInfo locations = RunTimeLoader.GetDirectory( "Scan Locations" );
+            DirectoryInfo locations = RunTimeLoader.GetDirectory("Scan Locations");
             if (locations.Exists)
             {
                 // Get all files
-                List<FileInfo> files = new List<FileInfo>( locations.GetFiles( "*.dss" ) );
+                List<FileInfo> files = new(locations.GetFiles("*.dss"));
 
                 // See if default exists
-                FileInfo builtIn = files.Find( f => (0 == string.Compare( "BuiltIn", Path.GetFileNameWithoutExtension( f.FullName ), true )) );
+                var builtIn = files.Find(f => 0 == string.Compare("BuiltIn", Path.GetFileNameWithoutExtension(f.FullName), true));
 
                 // Move to head
                 if (null != builtIn)
                 {
                     // Remove
-                    files.Remove( builtIn );
+                    files.Remove(builtIn);
 
                     // Insert
-                    files.Insert( 0, builtIn );
+                    files.Insert(0, builtIn);
                 }
 
                 // Process all
@@ -213,21 +198,21 @@ namespace JMS.DVB
                     try
                     {
                         // Load file
-                        ScanLocations sources = Load( extension );
+                        ScanLocations sources = Load(extension);
 
                         // Map with all unique identifiers found
-                        Dictionary<string, bool> map = new Dictionary<string, bool>();
+                        Dictionary<string, bool> map = new();
 
                         // Fill in
                         foreach (ScanLocation location in sources.Locations)
-                            if (!string.IsNullOrEmpty( location.UniqueName ))
+                            if (!string.IsNullOrEmpty(location.UniqueName))
                                 map[location.UniqueName] = true;
 
                         // Remove
-                        product.Locations.RemoveAll( l => !string.IsNullOrEmpty( l.UniqueName ) && map.ContainsKey( l.UniqueName ) );
+                        product.Locations.RemoveAll(l => !string.IsNullOrEmpty(l.UniqueName) && map.ContainsKey(l.UniqueName));
 
                         // Add all groups
-                        product.Locations.AddRange( sources.Locations );
+                        product.Locations.AddRange(sources.Locations);
                     }
                     catch
                     {
@@ -246,11 +231,7 @@ namespace JMS.DVB
         /// <typeparam name="T">Die gewünschte Art des Ursprungs.</typeparam>
         /// <param name="uniqueName">Der eindeutige Name des Ursprungs.</param>
         /// <returns>Der Ursprung oder <i>null</i>, wenn kein Ursprung mit diesem Namen existiert.</returns>
-        public T Find<T>( string uniqueName ) where T : ScanLocation
-        {
-            // Forward
-            return Find<T>( l => Equals( l.UniqueName, uniqueName ) );
-        }
+        public T? Find<T>(string uniqueName) where T : ScanLocation => Find<T>(l => Equals(l.UniqueName, uniqueName));
 
         /// <summary>
         /// Ermittelt einen Ursprung einer bestimmten Art.
@@ -258,11 +239,7 @@ namespace JMS.DVB
         /// <typeparam name="T">Die gwünschte Art des Ursprungs.</typeparam>
         /// <param name="predicate">Eine Methode zur Ermittelung des gewünschten Ursprungs.</param>
         /// <returns>Der gewünschte Ursprung oder <i>null</i>, wenn kein Ursprung der gewünschten Art existiert.</returns>
-        public T Find<T>( Predicate<T> predicate ) where T : ScanLocation
-        {
-            // Forward
-            return (T) Locations.Find( l => (l.GetType() == typeof( T )) && predicate( (T) l ) );
-        }
+        public T? Find<T>(Predicate<T> predicate) where T : ScanLocation => (T?)Locations.Find(l => (l.GetType() == typeof(T)) && predicate((T)l));
 
         /// <summary>
         /// Meldet die Standardkonfiguration für den Sendersuchlauf.
@@ -287,11 +264,7 @@ namespace JMS.DVB
         /// Erzeugt eine exakte Kopie.
         /// </summary>
         /// <returns>Die exakte Kopie dieser Instanz.</returns>
-        object ICloneable.Clone()
-        {
-            // Forward
-            return Clone();
-        }
+        object ICloneable.Clone() => Clone();
 
         #endregion
     }
