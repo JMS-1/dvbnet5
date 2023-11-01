f08e3cd259b86fcd93395cc2e6b37817179a498e
Dr. Jochen Manns <vcrnet-1@psimarron.net>
2023-11-01 10:49:57 +0100

    Started evaluation application.

---
+++ TestApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using JMS.DVB.CardServer;
+
+public static class Program
+{
+    public static void Main()
+    {
+        using var server = ServerImplementation.CreateInMemory();
+
+        var request = server.BeginSetProfile("Karlchen", false, false, false);
+
+        Console.WriteLine(request.AsyncWaitHandle.WaitOne());
+        Console.WriteLine(request.IsCompleted);
+        Console.WriteLine(request.AsyncState);
+
+        Console.WriteLine("DVB.NET");
+    }
+}
