1ce99f321c422b0e0472fd422847163212842a24
Dr. Jochen Manns <dev-2@psimarron.net>
2024-05-05 11:41:25 +0200

    ESLint clean.

--- WebClient/.eslintrc.js
+++ WebClient/.eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
     },
     rules: {
         '@typescript-eslint/explicit-function-return-type': 'off',
+        'react/no-unescaped-entities': 'off',
         'unused-imports/no-unused-imports-ts': 'warn',
         'unused-imports/no-unused-vars-ts': 'off',
     },
